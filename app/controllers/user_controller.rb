f17b8a4b6b5ba17adc2323fa1bbb5338bcaac3e7
SummerDAway <summerdaway@gmail.com>
2013-11-10 08:32:06 -0800

    Sorry for forgetting so many things.....
    Signed-off-by: SummerDAway <summerdaway@gmail.com>

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,25 @@
+class UserController < ApplicationController
+	def login
+		if params[:user] != nil then
+			@current_user = User.find_by_user_name(params[:user][:username])
+			if @current_user != nil && @current_user.user_pass == params[:user][:password] then
+				session[:current_user] = params[:user]
+				session[:is_admin] = @current_user.is_admin
+				flash[:notice] = "#{session[:is_admin]}"
+				redirect_to "/#{params[:ver]}/#{@current_user.user_name}/apps"
+			else
+				flash[:notice] = "Invalid username/password!"
+				redirect_to "/#{params[:ver]}/index"
+			end
+		else
+			flash[:notice] = "username/password should not be empty!"
+			redirect_to "/#{params[:ver]}/index"
+		end
+	end
+	
+	def logout
+		session[:current_user] = session[:is_admin] = nil
+		flash[:notice] = "Logout succeeded!"
+		redirect_to "/#{params[:ver]}/index"
+	end
+end
