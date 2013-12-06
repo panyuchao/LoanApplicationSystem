48bc134af60354fcc1dfaa7de71d3b151107b1ca
panyuchao <panyuchao@gmail.com>
2013-12-06 00:13:13 -0800

    modefy admin's UI
    Signed-off-by: panyuchao <panyuchao@gmail.com>

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -51,23 +51,23 @@ class AppsController < ApplicationController
 		# default: render 'new' template
 	end
 
-        def delete
-                times = params[:details]
-                delist = {}
-                App.all.each do |a|
-                    if a.created_at.to_i.to_s == times
-                        delist = a
-                    end
-                end
-                #delist = App.find_by_created_at(times.to_i)
-                if(delist == nil || delist == {} || ( delist[:applicant] != session[:current_user][:username] && session[:is_admin] == false) )
-                    flash[:notice] = "No permission"
-                    redirect_to "/#{params[:ver]}/#{session[:current_user][:username]}/apps" and return
-                end
-                App.destroy(delist)
-                flash[:notice] = "#{times} has been removed."
-                redirect_to "/#{params[:ver]}/#{session[:current_user][:username]}/apps"
+  def delete
+    times = params[:details]
+    delist = {}
+    App.all.each do |a|
+        if a.created_at.to_i.to_s == times
+            delist = a
         end
+    end
+    #delist = App.find_by_created_at(times.to_i)
+    if(delist == nil || delist == {} || ( delist[:applicant] != session[:current_user][:username] && session[:is_admin] == false) )
+        flash[:notice] = "No permission"
+        redirect_to "/#{params[:ver]}/#{session[:current_user][:username]}/apps" and return
+    end
+    App.destroy(delist)
+    flash[:notice] = "#{times} has been removed."
+    redirect_to "/#{params[:ver]}/#{session[:current_user][:username]}/apps"
+  end
 
 
 	def edit
@@ -89,8 +89,8 @@ class AppsController < ApplicationController
 		if check_username then return end
 		@current_user = User.find_by_user_name(session[:current_user][:username])
 		if @current_user.is_admin then
-			@get_apps = App.find(:all, :conditions => {:check_status => [1, 2]})
-			@check_status_num = App.get_check_status_num
+			@get_forms = Form.find(:all, :conditions => {:check_status => [1, 2]})
+			@check_status_num = Form.get_check_status_num
 		else
 			# current user is not admin
 			# this situation shouldn't happen
@@ -105,28 +105,30 @@ class AppsController < ApplicationController
 		@current_user = User.find_by_user_name(session[:current_user][:username])
 		statusx = params[:s0].to_i
 		statusy = params[:s1].to_i
+		flash[:notice] = params[:form_entry]
+#		redirect_to "/#{params[:ver]}/#{session[:current_user][:username]}/wait_for_verify" and return
 		if @current_user.is_admin then
 			if bad_change_status(statusx, statusy) then
 				flash[:notice] = "Status#{statusx} cannot change to Status#{statusy}"
 				redirect_to "/#{params[:ver]}/#{session[:current_user][:username]}/apps" and return
 			end
-			@app_now = App.find(params[:id])
-			if @app_now == nil then
-				flash[:notice] = "App with id{#{params[:id]} doesn't exist!"
-				redirect_to "/#{params[:ver]}/#{session[:current_user][:username]}/#{App.get_admin_tags[(statusx+1)>>1][1]}" and return
+			@form_now = Form.find(params[:id])
+			if @form_now == nil then
+				flash[:notice] = "Form with id{#{params[:id]} doesn't exist!"
+				redirect_to "/#{params[:ver]}/#{session[:current_user][:username]}/#{Form.get_admin_tags[(statusx+1)>>1][1]}" and return
 			end
 			if statusx == 1 && statusy == 3 then
 				if params[:account] == nil || params[:account][params[:id]] == "" then
 					flash[:notice] = "#{params[:account]}    Account number should not be empty"
-					redirect_to "/#{params[:ver]}/#{session[:current_user][:username]}/#{App.get_admin_tags[(statusx+1)>>1][1]}" and return
+					redirect_to "/#{params[:ver]}/#{session[:current_user][:username]}/#{Form.get_admin_tags[(statusx+1)>>1][1]}" and return
 				else
-					@app_now.account_num = params[:account][params[:id]]
+					@form_now.account_num = params[:account][params[:id]]
 				end
 			end
-			@app_now.check_status = statusy
-			@app_now.save!
+			@form_now.check_status = statusy
+			@form_now.save!
 			flash[:notice] = "操作成功"
-			redirect_to "/#{params[:ver]}/#{session[:current_user][:username]}/#{App.get_admin_tags[(statusx+1)>>1][1]}"
+			redirect_to "/#{params[:ver]}/#{session[:current_user][:username]}/#{Form.get_admin_tags[(statusx+1)>>1][1]}"
 		else
 			# current user is not admin
 			# this situation shouldn't happen
@@ -143,8 +145,8 @@ class AppsController < ApplicationController
 		if check_username then return end 
 		@current_user = User.find_by_user_name(session[:current_user][:username])
 		if @current_user.is_admin then
-			@get_apps = App.find(:all, :conditions => {:check_status => [3,4]})
-			@check_status_num = App.get_check_status_num
+			@get_forms = Form.find(:all, :conditions => {:check_status => [3,4]})
+			@check_status_num = Form.get_check_status_num
 			render "admin_reviewed"
 		else
 		end
