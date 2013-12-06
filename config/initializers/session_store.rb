e7f01065a327a976c50a7f2a2dbde532febba670
summerdaway <summerdaway@gmail.com>
2013-12-05 22:26:54 -0800

    session_migration
    Signed-off-by: summerdaway <summerdaway@gmail.com>

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,4 +5,4 @@ LoanApplicationSystem::Application.config.session_store :cookie_store, key: '_Lo
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# LoanApplicationSystem::Application.config.session_store :active_record_store
+  LoanApplicationSystem::Application.config.session_store :active_record_store
