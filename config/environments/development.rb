0dcb4af6381a4324cca5d1ec1251e3dcc7f06e32
Hao Tianyi <haotianyi0@126.com>
2013-12-25 21:47:28 +0800

    test on heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ LoanApplicationSystem::Application.configure do
   config.action_mailer.perform_deliveries = true  
   config.action_mailer.raise_delivery_errors = true  
   config.action_mailer.default :charset => "utf-8"  
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }  
+  #config.action_mailer.default_url_options = { :host => 'localhost:3000' }  
   ActionMailer::Base.smtp_settings = {    
     :address => "smtp.163.com",  
     :port => 25,  
