21da70d46c6f9f7b1e0d9a88c1115dea985ef3ed
Hao Tianyi <haotianyi0@126.com>
2013-12-25 21:56:52 +0800

    test on heroku

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -122,6 +122,7 @@ class AppsController < ApplicationController
 				flash[:notice] = "Form with id{#{params[:id]} doesn't exist!"
 				redirect_to "/#{params[:ver]}/#{session[:current_user][:username]}/#{Form.get_admin_tags[(statusx+1)>>1][1]}" and return
 			end
+			send_email(@form_now.id, statusx, statusy)
 			if statusx == 1 && statusy == 3 then
 				@form_now.apps.each do |appi|
 					appi.account_num = params[:form_entry][appi.id.to_s][:account_num].to_s
