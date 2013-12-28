5cc600d89959ad05bbb8bb0eaee82c4ef894cf78
Hao Tianyi <haotianyi0@126.com>
2013-12-28 17:23:58 +0800

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131228075356) do
+ActiveRecord::Schema.define(:version => 20131226075347) do
 
   create_table "apps", :force => true do |t|
     t.string   "details"
@@ -23,24 +23,7 @@ ActiveRecord::Schema.define(:version => 20131228075356) do
     t.datetime "updated_at",  :null => false
   end
 
-  create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority",   :default => 0, :null => false
-    t.integer  "attempts",   :default => 0, :null => false
-    t.text     "handler",                   :null => false
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
-  end
-
-  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
-
   create_table "forms", :force => true do |t|
-    t.string   "applicant"
     t.string   "app_type"
     t.float    "tot_amount"
     t.integer  "check_status"
